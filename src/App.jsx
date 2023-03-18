dca786edb45bc1ad61fd25e9ebdc5d0bdfc2eb3c
Byron <bgva2005@yahoo.com>
2023-03-17 22:02:09 -0500

    feat: navbar add navbar's css and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import BasicExample from "./components/NavBar";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <BasicExample />
+      <NavBar />
     </div>
   );
 }
